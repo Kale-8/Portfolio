0ec02de5c82b10ab8fe7f37a8c4831194aa93a50
Kale-8 <kaled.geme2@gmail.com>
2025-05-25 21:17:34 -0500

    rickroll :)

--- CV/main.js
+++ CV/main.js
@@ -4,3 +4,8 @@ document.querySelectorAll('.card h2+i').forEach(tag => {
         tag.parentElement.parentElement.classList.toggle('active');
     });
 });
+
+document.querySelector('#html > div > i').addEventListener('click', () => {
+    const iframe = document.querySelector('iframe');
+    iframe.src = `${iframe.src}?autoplay=1`;
+});
